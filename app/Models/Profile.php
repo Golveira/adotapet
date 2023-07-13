86afd4b8fa8e0d23cdfc123983534963c3174ec6
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-12 23:46:54 -0300

    Add fillable fields in Profile model

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'website',
+        'whatsapp',
+        'state',
+        'city',
+        'avatar',
+        'bio'
+    ];
 }
