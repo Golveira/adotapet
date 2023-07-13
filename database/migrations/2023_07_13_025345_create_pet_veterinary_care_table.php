17537402c57f1fd37f1c44fbe9057526e6e38684
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-13 10:20:07 -0300

    Add foreign keys to pivot tables

--- database/migrations/2023_07_13_025345_create_pet_veterinary_care_table.php
+++ database/migrations/2023_07_13_025345_create_pet_veterinary_care_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,8 @@ return new class extends Migration
     {
         Schema::create('pet_veterinary_care', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('pet_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('veterinary_care_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
