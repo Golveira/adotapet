e70c4f2e266d01be623c0e51161c8ace38e0b8e5
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-13 10:59:12 -0300

    Change DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,11 +17,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
             SociabilitySeeder::class,
             TemperamentSeeder::class,
             VeterinaryCareSeeder::class,
-            PetSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
