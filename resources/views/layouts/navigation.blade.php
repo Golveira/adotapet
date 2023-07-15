0a0e6098bcb7bb3091798a6c564c60c0b6d00777
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-15 17:44:49 -0300

    Update navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,9 +6,8 @@
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
         </a>
 
-        <!-- User menu dropdown -->
-        @auth
-            <div class="flex items-center md:order-2">
+        <div class="flex items-center md:order-2">
+            @auth
                 <button type="button"
                     class="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                     id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
@@ -17,13 +16,14 @@
                     <img class="w-8 h-8 rounded-full" src="{{ asset('assets/images/user.png') }}" alt="user photo">
                 </button>
 
-                <!-- Dropdown menu -->
+                <!-- User menu dropdown -->
                 <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
                     id="user-dropdown">
                     <div class="px-4 py-3">
                         <span class="block text-sm text-gray-900 dark:text-white">
                             {{ Auth::user()->name }}
                         </span>
+
                         <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">
                             {{ Auth::user()->email }}
                         </span>
@@ -61,29 +61,37 @@
                         </li>
                     </ul>
                 </div>
+            @endauth
 
-                <button data-collapse-toggle="navbar-user" type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-user" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
-            </div>
-        @else
-            <div class="flex items-center md:order-2">
-                <x-primary-link-button href="{{ route('login') }}">
-                    {{ __('Login') }}
-                </x-primary-link-button>
+            @guest
+                <div class="hidden lg:block">
+                    <x-primary-link-button href="{{ route('login') }}" class="me-3">
+                        {{ __('Login') }}
+                    </x-primary-link-button>
 
-                <x-secondary-link-button href="{{ route('register') }}">
-                    {{ __('Register') }}
-                </x-secondary-link-button>
-            </div>
-        @endauth
+                    <x-secondary-link-button href="{{ route('register') }}">
+                        {{ __('Register') }}
+                    </x-secondary-link-button>
+                </div>
+
+                <div class="md:hidden me-5">
+                    <a href="{{ route('login') }}">
+                        <x-icon-user />
+                    </a>
+                </div>
+            @endguest
+
+            <button data-collapse-toggle="navbar-user" type="button"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-user" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 1h15M1 7h15M1 13h15" />
+                </svg>
+            </button>
+        </div>
 
         <!-- Nav links -->
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
