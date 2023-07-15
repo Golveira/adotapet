bb4dac4fe062dcf48c97323dbdcdb3084bac6e18
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-15 13:34:13 -0300

    Add WelcomeController

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pet;
+
+class WelcomeController extends Controller
+{
+    public function __invoke()
+    {
+        $pets = Pet::latest()
+            ->limit(8)
+            ->get(['name', 'state', 'city', 'main_photo']);
+
+        return view('welcome', compact('pets'));
+    }
+}
