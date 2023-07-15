7b1489c1127ce02df828b4ac124484df629f7b22
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-15 15:17:25 -0300

    Add states and cities table and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,12 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Database\Seeders\PetSeeder;
 use Illuminate\Database\Seeder;
+use Database\Seeders\CityTableSeeder;
+use Database\Seeders\StateTableSeeder;
 use Database\Seeders\SociabilitySeeder;
 use Database\Seeders\TemperamentSeeder;
 use Database\Seeders\VeterinaryCareSeeder;
+use Guiliredu\BrazilianCityMigrationSeed\Database\Seeds\CitySeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +20,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            StateTableSeeder::class,
+            CityTableSeeder::class,
             SociabilitySeeder::class,
             TemperamentSeeder::class,
             VeterinaryCareSeeder::class,
