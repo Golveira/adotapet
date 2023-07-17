e9da1e975fc9f1df14be6ddf07a6d16861408982
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-17 16:52:44 -0300

    Update font size in badge component

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -5,6 +5,6 @@
     $text = "text-{$color}-800 ";
 @endphp
 
-<span {{ $attributes->merge(['class' => $bg . $text . 'text-base font-medium mr-2 px-2.5 py-0.5 rounded']) }}>
+<span {{ $attributes->merge(['class' => $bg . $text . 'text-sm font-medium mr-2 px-2.5 py-0.5 rounded']) }}>
     {{ $slot }}
 </span>
