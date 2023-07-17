ebef117a01dfeb69323fc01cfd4437cbfd8f95eb
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-17 17:36:16 -0300

    Add favorite-link

---
+++ resources/views/components/favorite-link.blade.php
@@ -0,0 +1,4 @@
+<a href="#"
+    {{ $attributes->merge(['class' => 'bg-white text-dark-700 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center']) }}>
+    <x-icons.heart-outline />
+</a>
