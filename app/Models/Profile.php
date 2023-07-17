df1b93fa2cffc9b1431974f57e2faaf3c3ba50ff
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-17 12:10:01 -0300

    Add getAddressAttribute to Profile model

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -24,4 +24,9 @@ class Profile extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function getAddressAttribute(): string
+    {
+        return "{$this->city}, {$this->state}";
+    }
 }
