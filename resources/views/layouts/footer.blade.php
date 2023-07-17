4727d597fe4cba28943c84f330efc883e4fe1e0c
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-17 16:45:47 -0300

    Update icon names in the footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -98,15 +98,15 @@
             <!-- Social media -->
             <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                 <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                    <x-icon-facebook />
+                    <x-icons.facebook />
                 </a>
 
                 <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                    <x-icon-instagram />
+                    <x-icons.instagram />
                 </a>
 
                 <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                    <x-icon-twitter />
+                    <x-icons.twitter />
                 </a>
             </div>
         </div>
