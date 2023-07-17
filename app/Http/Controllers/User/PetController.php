3e34cd491f77fb1054c75ab32f46008da056b968
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-17 17:04:56 -0300

    Add show method to PetController

---
+++ app/Http/Controllers/User/PetController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Models\Pet;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PetController extends Controller
+{
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Pet $pet): View
+    {
+        $pet->load('user.profile', 'sociabilities', 'temperaments', 'veterinaryCares');
+
+        return view('pets.show', compact('pet'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
