881a3e9a9da222655c28d6943bd1966a62c9e419
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-18 12:42:53 -0300

    Update links of pet-card component

--- resources/views/components/pet-card.blade.php
+++ resources/views/components/pet-card.blade.php
@@ -2,15 +2,15 @@
 
 <div href="#" class="block relative overflow-hidden rounded-lg bg-cover bg-no-repeat h-72"
     style="background-image: url({{ $pet->main_photo }});">
-    <a href="#div" class="absolute inset-0 bg-gradient-to-t from-black opacity-70 rounded-lg"></a>
-
-    <a href="#p"
-        class="absolute top-3 right-3 flex items-center justify-center text-white border border-white hover:bg-red-700 hover:border-red-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center">
-        <x-icon-heart class="w-5 h-5" />
+    <a href="{{ route('pets.show', $pet->id) }}"
+        class="absolute inset-0 bg-gradient-to-t from-black opacity-70 rounded-lg">
     </a>
 
+    <x-favorite-link class="absolute top-3 right-3 " />
+
     <div class="absolute bottom-0 inset-x-0 ml-5 mb-5">
-        <a href="#" class="font-bold text-xl text-white dark:text-blue-500 hover:underline">
+        <a href="{{ route('pets.show', $pet->id) }}"
+            class="font-bold text-xl text-white dark:text-blue-500 hover:underline">
             {{ $pet->name }}
         </a>
 
