98a227d09a562f7928edb9a6f691366b330732e2
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-22 11:11:57 -0300

    Add profile show view

---
+++ resources/views/profile/show.blade.php
@@ -0,0 +1,64 @@
+<x-app-layout>
+    <x-card class="mb-10">
+        <div class="grid lg:grid-cols-2 mb-3 gap-4">
+            <div class="flex items-center">
+                <x-avatar :avatar="$user->avatar" class="w-20 h-20 rounded-full" />
+
+                <div>
+                    <h3 class="ms-5 text-2xl font-bold">{{ $user->name }}</h3>
+
+                    <div class="ms-5 text-sm text-gray-700">
+                        {{ $user->profile->location }}
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                @if ($user->profile->website)
+                    <div class="flex mb-2">
+                        <span class="text-blue-700 mr-3">
+                            <x-icons.globe />
+                        </span>
+
+                        <x-link href="{{ $user->profile->website }}"
+                            class="text-sm text-gray-700 hover:no-underline hover:text-blue-700">
+                            {{ $user->profile->website }}
+                        </x-link>
+                    </div>
+                @endif
+
+                @if ($user->profile->whatsapp)
+                    <div class="flex mb-2">
+                        <span class="text-blue-700 mr-3">
+                            <x-icons.whatsapp />
+                        </span>
+
+                        <x-link href="https://wa.me/{{ $user->profile->whatsapp }}"
+                            class="text-sm text-gray-700 hover:no-underline hover:text-blue-700">
+                            {{ $user->profile->whatsapp }}
+                        </x-link>
+                    </div>
+                @endif
+
+                @if ($user->email)
+                    <div class="flex mb-2">
+                        <span class="text-blue-700 mr-3">
+                            <x-icons.envelope />
+                        </span>
+
+                        <x-link href="mailto:{{ $user->email }}"
+                            class="text-sm text-gray-700 hover:no-underline hover:text-blue-700">
+                            {{ $user->email }}
+                        </x-link>
+                    </div>
+                @endif
+            </div>
+        </div>
+
+        <div class="text-gray-700">
+            {{ $user->profile->bio }}
+        </div>
+    </x-card>
+
+    <livewire:show-pets :userId="$user->id" />
+</x-app-layout>
