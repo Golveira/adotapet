e5943559542a88835204301f40d86bbfbf48ffa4
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-22 12:00:03 -0300

    Remove container from app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,9 +37,7 @@
 
         <!-- Page Content -->
         <main>
-            <div class="max-w-screen-xl px-4 py-8 mx-auto">
-                {{ $slot }}
-            </div>
+            {{ $slot }}
         </main>
 
         @include('layouts.footer')
