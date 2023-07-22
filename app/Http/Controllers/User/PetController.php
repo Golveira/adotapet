888999d9df15c72cf1c75034d7bd13815735d3da
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-22 08:58:04 -0300

    Update loaded data in show method of PetController

--- app/Http/Controllers/User/PetController.php
+++ app/Http/Controllers/User/PetController.php
@@ -27,7 +27,14 @@ class PetController extends Controller
 
     public function show(Pet $pet): View
     {
-        $pet->load('user', 'city', 'state', 'sociabilities', 'temperaments', 'veterinaryCares');
+        $pet->load(
+            'user:id,name,username',
+            'city:id,title',
+            'state:id,letter',
+            'sociabilities:id,name',
+            'temperaments:id,name',
+            'veterinaryCares:id,name'
+        );
 
         return view('pets.show', compact('pet'));
     }
