0d151f7e0f9d3800392df541ca28e09c2240b145
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-22 11:08:19 -0300

    Update pet-card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,6 @@
-<div {!! $attributes->merge([
-    'class' => 'p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700',
-]) !!}>
+<div
+    {{ $attributes->merge([
+        'class' => 'p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700',
+    ]) }}>
     {{ $slot }}
 </div>
