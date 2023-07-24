2452253d65e63429bb689f57e0651f9526af3c4e
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-24 11:22:16 -0300

    Remove margin bottom from city-state-select component

--- resources/views/livewire/city-state-select.blade.php
+++ resources/views/livewire/city-state-select.blade.php
@@ -1,5 +1,5 @@
 <div class="grid lg:grid-cols-2 gap-4">
-    <div class="mb-5">
+    <div>
         <x-input-label class="mb-3 text-lg" for="state" :value="__('State')" />
         <x-select wire:model="stateId" id="state" name="state_id">
             <option value>{{ __('Select the state') }}</option>
@@ -12,7 +12,7 @@
         </x-select>
     </div>
 
-    <div class="mb-5">
+    <div>
         <x-input-label class="mb-3 text-lg" for="city" :value="__('City')" />
         <x-select wire:model.defer="cityId" id="city" name="city_id">
             <option value>{{ __('Select the city') }}</option>
