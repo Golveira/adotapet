1f7a9d3b5febad98101be54995911163904188e7
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-25 11:39:33 -0300

    Refactor badge component

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,10 +1,18 @@
-@props(['color' => 'gray'])
+@props(['color' => 'default'])
 
 @php
-    $bg = "bg-{$color}-100 ";
-    $text = "text-{$color}-800 ";
+    $classes = [
+        'default' => 'bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
+        'dark' => 'bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
+        'red' => 'bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
+        'green' => 'bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
+        'yellow' => 'bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
+        'indigo' => 'bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
+        'purple' => 'bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
+        'pink' => 'bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
+    ][$color];
 @endphp
 
-<span {{ $attributes->merge(['class' => $bg . $text . 'text-sm font-medium mr-2 px-2.5 py-0.5 rounded']) }}>
+<span {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </span>
