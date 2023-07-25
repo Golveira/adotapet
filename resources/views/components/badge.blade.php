691e2788f3a29337e981affa72b36ae3bb02ba24
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-25 12:30:48 -0300

    Refactor badge component

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -2,17 +2,17 @@
 
 @php
     $classes = [
-        'default' => 'bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
-        'dark' => 'bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
-        'red' => 'bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
-        'green' => 'bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
-        'yellow' => 'bg-yellow-100 text-yellow-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
-        'indigo' => 'bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
-        'purple' => 'bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
-        'pink' => 'bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded',
+        'default' => 'bg-blue-100 text-blue-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'dark' => 'bg-gray-100 text-gray-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'red' => 'bg-red-100 text-red-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'green' => 'bg-green-100 text-green-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'yellow' => 'bg-yellow-100 text-yellow-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'indigo' => 'bg-indigo-100 text-indigo-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'purple' => 'bg-purple-100 text-purple-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'pink' => 'bg-pink-100 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded',
     ][$color];
 @endphp
 
-<span {{ $attributes->merge(['class' => $classes]) }}>
+<span {{ $attributes->merge(['class' => ' ' . $classes]) }}>
     {{ $slot }}
 </span>
