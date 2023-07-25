d01f4b094c45e7a5fb4077fd65517bc967ad43fb
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-25 11:13:33 -0300

    Add setup method for TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use Database\Seeders\CityTableSeeder;
+use Database\Seeders\SociabilitySeeder;
+use Database\Seeders\StateTableSeeder;
+use Database\Seeders\TemperamentSeeder;
+use Database\Seeders\VeterinaryCareSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed(StateTableSeeder::class);
+        $this->seed(CityTableSeeder::class);
+    }
 }
