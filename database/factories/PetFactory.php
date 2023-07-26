7289f169197e276e5025a0691aeb97665fe89142
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-26 12:25:00 -0300

    Refactor PetFactory

--- database/factories/PetFactory.php
+++ database/factories/PetFactory.php
@@ -2,8 +2,11 @@
 
 namespace Database\Factories;
 
-use App\Models\State;
 use App\Models\User;
+use App\Models\State;
+use App\Models\Sociability;
+use App\Models\Temperament;
+use App\Models\VeterinaryCare;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -34,5 +37,62 @@ class PetFactory extends Factory
             'is_adopted' => false,
             'is_visible' => true,
         ];
+    }
+
+    public function withSociabilities(): self
+    {
+        return $this->afterCreating(function ($pet) {
+            $sociabilities = Sociability::pluck('id')->toArray();
+
+            $pet->sociabilities()->attach($sociabilities);
+        });
+    }
+
+    public function withVeterinaryCares(): self
+    {
+        return $this->afterCreating(function ($pet) {
+            $veterinaryCares = VeterinaryCare::pluck('id')->toArray();
+
+            $pet->veterinaryCares()->attach($veterinaryCares);
+        });
+    }
+
+    public function withTemperaments(): self
+    {
+        return $this->afterCreating(function ($pet) {
+            $temperaments = Temperament::pluck('id')->toArray();
+
+            $pet->temperaments()->attach($temperaments);
+        });
+    }
+
+    public function withMedia(): self
+    {
+        return $this->afterCreating(function ($pet) {
+            $images = [
+                'dog' => [
+                    'main' => 'assets/images/dog.jpg',
+                    'gallery' => [
+                        'assets/images/dog2.jpg',
+                        'assets/images/dog3.jpg',
+                    ],
+                ],
+                'cat' => [
+                    'main' => 'assets/images/cat.jpg',
+                    'gallery' => [
+                        'assets/images/cat2.jpg',
+                        'assets/images/cat3.jpg',
+                    ],
+                ]
+            ];
+
+            $pet->addMediaFromUrl(asset($images[$pet->specie]['main']))
+                ->toMediaCollection('pets');
+
+            foreach ($images[$pet->specie]['gallery'] as $image) {
+                $pet->addMediaFromUrl(asset($image))
+                    ->toMediaCollection('pets-gallery');
+            }
+        });
     }
 }
