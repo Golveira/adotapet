632c94249983fcb68858c5aad4f7058cdc200804
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-26 10:04:27 -0300

    Refactor veterinary-cares-checkbox component

--- resources/views/components/veterinary-cares-checkboxes.blade.php
+++ resources/views/components/veterinary-cares-checkboxes.blade.php
@@ -1,8 +1,10 @@
+@props(['checkedValues' => []])
+
 <div class="flex flex-wrap">
-    @foreach ($veterinaryCares as $care)
+    @foreach ($veterinaryCares as $vetCare)
         <div class="mb-3 mr-3">
-            <x-checkbox id="{{ $care->name }}" value="{{ $care->id }}" :label="$care->name" name="veterinary_cares[]"
-                checked="{{ is_array(old('veterinary_cares')) and in_array($care->id, old('veterinary_cares')) }}" />
+            <x-advanced-checkbox id="{{ $vetCare->name }}" name="veterinary_cares[]" value="{{ $vetCare->id }}"
+                :label="$vetCare->name" checked="{{ in_array($vetCare->id, $checkedValues) }}" />
         </div>
     @endforeach
 </div>
