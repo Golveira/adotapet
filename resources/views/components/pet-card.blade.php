84c19ec896fba41b3884e38cce948ed4b72028ec
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-28 10:27:46 -0300

    Add hover animation and transition to pet-card component

--- resources/views/components/pet-card.blade.php
+++ resources/views/components/pet-card.blade.php
@@ -1,12 +1,13 @@
 @props(['pet' => null])
 
-<div href="#" class="block relative overflow-hidden rounded-lg bg-cover bg-no-repeat h-72"
+<div href="#"
+    class="group block relative overflow-hidden rounded-lg bg-cover bg-no-repeat h-72 hover:scale-[1.02] transition-all duration-200"
     style="background-image: url({{ $pet->main_photo }});">
-    <a href="{{ route('pets.show', $pet->id) }}"
-        class="absolute inset-0 bg-gradient-to-t from-black opacity-70 rounded-lg">
+
+    <a href="{{ route('pets.show', $pet->id) }}" class="absolute inset-0 bg-gradient-to-t from-black opacity-60">
     </a>
 
-    <x-favorite-link class="absolute top-3 right-3 " />
+    <x-favorite-link class="absolute top-3 right-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
 
     <div class="absolute bottom-0 inset-x-0 ml-5 mb-5">
         <a href="{{ route('pets.show', $pet->id) }}"
