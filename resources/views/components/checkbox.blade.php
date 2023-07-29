55d8a0c1979bc52e7f5e9e1fe7aa7d1940e03b99
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-29 13:00:32 -0300

    Add checkbox component

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,8 +1,8 @@
-@props(['label' => ''])
+@props(['label' => '', 'checked' => false])
 
 <input
     {{ $attributes->merge(['class' => 'w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2']) }}
-    type="checkbox" />
+    type="checkbox" {{ $checked ? 'checked' : '' }} />
 
 <label for="{{ $attributes['id'] }}" class="ml-2 text-sm font-medium text-gray-900">
     {{ __($label) }}
