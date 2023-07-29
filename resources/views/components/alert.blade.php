bbd42850f05c86b36d327d8e3643d491af1357b2
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-29 13:00:17 -0300

    Add alert component

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,7 @@
+@props(['message' => ''])
+
+<div {{ $attributes->merge(['class' => 'p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 ']) }} role="alert">
+    <span class="font-medium">
+        {{ $message }}
+    </span>
+</div>
