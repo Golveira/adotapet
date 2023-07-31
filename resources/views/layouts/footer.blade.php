174cce96a81fe777ee605729ff06255008d24721
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-07-31 11:11:45 -0300

    Remove string from translation helper in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -12,7 +12,7 @@
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                 <div>
                     <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                        {{ __('Pets') }}
+                        Pets
                     </h2>
 
                     <ul class="text-gray-600 dark:text-gray-400">
