4335fa50471b7ae533db9b8c9962bb3b6968f247
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-08-01 13:10:45 -0300

    Update WelcomeController's __invoke method

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Pet;
+use Illuminate\Support\Facades\Auth;
 
 class WelcomeController extends Controller
 {
     public function __invoke()
     {
+        $bookmarks = Auth::user()
+            ->bookmarks()
+            ->pluck('pets.id');
+
         $pets = Pet::latest()
             ->with(['media', 'city', 'state'])
             ->limit(8)
-            ->get();
+            ->get()
+            ->map(function ($pet) use ($bookmarks) {
+                $pet->is_bookmarked = $bookmarks->contains($pet->id);
+                return $pet;
+            });
 
         return view('welcome', compact('pets'));
     }
