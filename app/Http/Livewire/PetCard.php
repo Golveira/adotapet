b952c37138bdb2258644ea03cb46c028957c78cf
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-08-07 11:18:57 -0300

    Add PetCard livewire component and view

---
+++ app/Http/Livewire/PetCard.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Pet;
+use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+
+class PetCard extends Component
+{
+    public $pet;
+
+    public function mount(Pet $pet)
+    {
+        $this->pet = $pet;
+    }
+
+    public function toggleBookmark()
+    {
+        if (Auth::guest()) {
+            return redirect()->route('login');
+        }
+
+        Auth::user()->toggleBookmark($this->pet);
+    }
+
+    public function render()
+    {
+        $isBookmarked = Auth::user()?->hasBookmarked($this->pet);
+
+        return view('livewire.pet-card', compact('isBookmarked'));
+    }
+}
