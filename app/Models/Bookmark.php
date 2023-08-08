01b76c6859a78ca5c9762e3c81091a91c469ead7
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-08-08 11:34:34 -0300

    Add relations to Bookmark model

--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Bookmark extends Model
 {
     use HasFactory;
+
+    public function pet(): BelongsTo
+    {
+        return $this->belongsTo(Pet::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
