2e1cd6a826d6b6fc19b9d39b5b2f04638731464c
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-08-14 10:59:56 -0300

    Add alert component

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,7 +1,14 @@
-@props(['message' => ''])
+@props(['type' => 'success'])
 
-<div {{ $attributes->merge(['class' => 'p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 ']) }} role="alert">
-    <span class="font-medium">
-        {{ $message }}
-    </span>
+@php
+    $color = [
+        'success' => 'bg-green-100 text-green-800 border border-green-300',
+        'error' => 'bg-red-100 text-red-800 border border-red-300',
+        'warning' => 'bg-yellow-100 text-yellow-800 border  border-yellow-300',
+        'info' => 'bg-blue-100 text-blue-800 border border-blue-300',
+    ][$type];
+@endphp
+
+<div {{ $attributes->merge(['class' => "p-4 mb-4 text-sm rounded-lg $color"]) }} role="alert">
+    {{ $slot }}
 </div>
