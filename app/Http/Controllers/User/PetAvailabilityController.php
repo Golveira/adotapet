8bdf345828da2ca6421fcdc3846861a7013cc2b7
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-08-14 12:04:17 -0300

    Add PetAvailabilityController

---
+++ app/Http/Controllers/User/PetAvailabilityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Models\Pet;
+use App\Http\Controllers\Controller;
+
+class PetAvailabilityController extends Controller
+{
+    public function markAsAvailable(Pet $pet)
+    {
+        $pet->update(['is_adopted' => false]);
+
+        toast(__('pets.updated'), 'success');
+
+        return redirect()->back();
+    }
+
+    public function markAsAdopted(Pet $pet)
+    {
+        $pet->update(['is_adopted' => true]);
+
+        toast(__('pets.updated'), 'success');
+
+        return redirect()->back();
+    }
+}
