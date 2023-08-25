be13fe6a8ad2d6134550b4886c9cfd4056a9af54
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-08-25 12:02:51 -0300

    Remove email logic from ProfileService

--- app/Services/ProfileService.php
+++ app/Services/ProfileService.php
@@ -9,21 +9,14 @@ class ProfileService
 {
     public function update(array $profileData, $image, User $user)
     {
-        $user->fill([
+        $user->update([
             'name' => $profileData['name'],
-            'username' => $profileData['username'],
-            'email' => $profileData['email'],
+            'username' => $profileData['username']
         ]);
-
-        if ($user->isDirty('email')) {
-            $user->email_verified_at = null;
-        }
-
-        $user->save();
 
         $user->profile()->updateOrCreate(
             ['user_id' => $user->id],
-            Arr::except($profileData, ['name', 'username', 'email'])
+            Arr::except($profileData, ['name', 'username', 'avatar'])
         );
 
         if ($image) {
