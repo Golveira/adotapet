a1531d75358a23ad2c09bcd487ffc428bff082cf
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-08-25 11:48:20 -0300

    Extract password validation to UpdatePasswordRequest

--- app/Http/Controllers/Settings/PasswordUpdateController.php
+++ app/Http/Controllers/Settings/PasswordUpdateController.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Controllers\Settings;
 
+use App\Http\Requests\UpdatePasswordRequest;
 use Illuminate\View\View;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Validation\Rules\Password;
 
 class PasswordUpdateController extends Controller
 {
@@ -16,15 +15,10 @@ class PasswordUpdateController extends Controller
         return view('settings.password.edit');
     }
 
-    public function update(Request $request): RedirectResponse
+    public function update(UpdatePasswordRequest $request): RedirectResponse
     {
-        $validated = $request->validateWithBag('updatePassword', [
-            'current_password' => ['required', 'current_password'],
-            'password' => ['required', Password::defaults(), 'confirmed'],
-        ]);
-
         $request->user()->update([
-            'password' => Hash::make($validated['password']),
+            'password' => Hash::make($request->password),
         ]);
 
         toast(__('passwords.updated'), 'success');
