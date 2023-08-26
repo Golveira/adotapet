f2aff8b3aa9ecae0d05349d8b22ab956a8a3f1a7
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-08-26 12:31:49 -0300

    Remove unused classes from avatar-upload view

--- resources/views/livewire/avatar-upload.blade.php
+++ resources/views/livewire/avatar-upload.blade.php
@@ -1,14 +1,14 @@
 <div class="p-4 mb-6 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 ">
     <div class="flex gap-8">
-        <img class="mb-4 rounded-lg object-cover  w-28 h-28 sm:mb-0 xl:mb-4 2xl:mb-0" src="{{ $user->avatar }}"
+        <img class="mb-4 rounded-lg object-cover w-28 h-28 sm:mb-0 xl:mb-4 2xl:mb-0" src="{{ $user->avatar }}"
             alt="Avatar">
 
         <div>
-            <h3 class="mb-1 text-xl font-bold text-gray-900 dark:text-white">
+            <h3 class="mb-1 text-xl font-bold text-gray-900">
                 {{ __('Profile Picture') }}
             </h3>
 
-            <div class="mb-4 text-sm text-gray-500 dark:text-gray-400">
+            <div class="mb-4 text-sm text-gray-500">
                 JPG, JPEG or PNG. {{ __('Max size of') }} 2MB
             </div>
 
@@ -16,7 +16,7 @@
                 <span class="text-sm text-red-500">{{ $message }}</span>
             @enderror
 
-            <div class="mt-4 flex items-center space-x-4">
+            <div class="mt-4 flex flex-col md:flex-row gap-2">
                 <label for="photo"
                     class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 cursor-pointer">
                     <x-icons.upload />
@@ -26,7 +26,7 @@
                 <input wire:model="photo" type="file" class="hidden" name="photo" id="photo">
 
                 <button wire:click="removeAvatar" type="button"
-                    class="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                    class="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">
                     {{ __('Remove') }}
                 </button>
             </div>
