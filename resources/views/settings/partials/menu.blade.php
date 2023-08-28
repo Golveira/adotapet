0cff4fb350b208cd199df66fdaf53278a45ef782
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-08-28 12:19:11 -0300

    Update links of settings menu

--- resources/views/settings/partials/menu.blade.php
+++ resources/views/settings/partials/menu.blade.php
@@ -1,29 +1,20 @@
 <div class="col-span-full xl:col-auto mb-6">
     <div
         class="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        <a href="{{ route('settings.profile.edit') }}"
-            class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+        <x-list-group-link :href="route('settings.profile.edit')" :active="request()->routeIs('settings.profile.edit')">
             {{ __('Profile') }}
-        </a>
+        </x-list-group-link>
 
-        <a href="{{ route('settings.email.edit') }}"
-            class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+        <x-list-group-link :href="route('settings.email.edit')" :active="request()->routeIs('settings.email.edit')">
             {{ __('Email') }}
-        </a>
+        </x-list-group-link>
 
-        <a href="{{ route('settings.password.edit') }}"
-            class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+        <x-list-group-link :href="route('settings.password.edit')" :active="request()->routeIs('settings.password.edit')">
             {{ __('Password') }}
-        </a>
+        </x-list-group-link>
 
-        <a href="#privacy-settings"
-            class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+        <x-list-group-link href="#">
             {{ __('Notifications') }}
-        </a>
-
-        <a href="#privacy-settings"
-            class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
-            {{ __('Security') }}
-        </a>
+        </x-list-group-link>
     </div>
 </div>
