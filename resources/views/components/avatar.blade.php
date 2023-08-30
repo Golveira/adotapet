9c16703c3bc6f658d8911fc7ba2c88b077053ae8
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-08-30 11:15:56 -0300

    Refactor avatar component

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,3 +1,3 @@
 @props(['avatar' => null])
 
-<img {{ $attributes->merge(['class' => 'w-10 h-10 rounded-full']) }} src="{{ $avatar }}" alt="User avatar">
+<img {{ $attributes->merge(['class' => 'w-10 h-10 object-cover']) }} src="{{ $avatar }}" alt="User avatar">
