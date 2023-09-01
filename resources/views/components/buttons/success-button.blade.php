3cf47e92ad447bff1e2642186f8ce680ef1eeceb
Gabriel Oliveira <gabriel.golveira@gmail.com>
2023-09-01 12:22:05 -0300

    Add better organization for buttons components

---
+++ resources/views/components/buttons/success-button.blade.php
@@ -0,0 +1,18 @@
+@props(['outline' => false])
+
+@php
+    $classes = [
+        'solid' => 'focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800',
+        'outline' => 'text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800',
+    ][$outline ? 'outline' : 'solid'];
+@endphp
+
+@if ($attributes->has('href'))
+    <a {{ $attributes->merge(['class' => $classes]) }}>
+        {{ $slot }}
+    </a>
+@else
+    <button {{ $attributes->merge(['class' => $classes]) }}>
+        {{ $slot }}
+    </button>
+@endif
---
+++ resources/views/components/buttons/warning-button.blade.php
@@ -0,0 +1,18 @@
+@props(['outline' => false])
+
+@php
+    $classes = [
+        'solid' => 'focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900',
+        'outline' => 'text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900',
+    ][$outline ? 'outline' : 'solid'];
+@endphp
+
+@if ($attributes->has('href'))
+    <a {{ $attributes->merge(['class' => $classes]) }}>
+        {{ $slot }}
+    </a>
+@else
+    <button {{ $attributes->merge(['class' => $classes]) }}>
+        {{ $slot }}
+    </button>
+@endif
