ada804dc3dde0069518d6db886239a7344c723d7
Golveira <gabriel.golveira@gmail.com>
2023-09-07 10:51:03 -0300

    Add alpine js to modal-delete

--- resources/views/components/modal-delete.blade.php
+++ resources/views/components/modal-delete.blade.php
@@ -1,47 +1,46 @@
-@push('modals')
-    @props(['action' => null, 'identifier' => 'popup-modal', 'title' => 'Are you sure?'])
+@props(['title', 'action'])
 
-    <div id="{{ $identifier }}" tabindex="-1" backdropClasses="bg-gray-900 bg-opacity-50"
-        class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full bg-dark-700">
+<div x-data="{ open: false }">
+    <div>
+        {{ $slot }}
+    </div>
+
+    <div x-show="open" class="z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center">
+        <div x-on:click="open = false" class="absolute w-full h-full bg-gray-900 opacity-50">
+        </div>
 
         <div class="relative w-full max-w-md max-h-full">
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <button type="button"
-                    class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-hide="{{ $identifier }}">
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                    </svg>
-                    <span class="sr-only">{{ __('Close modal') }}</span>
+                <button x-on:click="open = false" type="button"
+                    class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                    <x-icons.close />
+
+                    <span class="sr-only">
+                        Close modal
+                    </span>
                 </button>
 
                 <div class="p-6 text-center">
-                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                    </svg>
+                    <x-icons.exclamation />
 
                     <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                        {{ $title }}
+                        {{ __($title) }}
                     </h3>
 
-                    <form action="{{ $action }}" method="POST" class="inline">
+                    <form action="{{ $action }}" method="post">
                         @csrf
-                        @method('DELETE')
+                        @method('delete')
 
-                        <x-button type="submit" color="red" data-modal-hide="{{ $identifier }}">
-                            {{ __('Yes, delete') }}
-                        </x-button>
+                        <x-buttons.secondary-button @click.prevent="open = false">
+                            {{ __('Cancel') }}
+                        </x-buttons.secondary-button>
+
+                        <x-buttons.danger-button x-on:click="open = false" type="submit">
+                            {{ __('Delete') }}
+                        </x-buttons.danger-button>
                     </form>
-
-                    <x-button color="alternative" data-modal-hide="{{ $identifier }}">
-                        {{ __('No, cancel') }}
-                    </x-button>
                 </div>
             </div>
         </div>
     </div>
-@endpush
+</div>
