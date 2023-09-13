759ed44e3119c45567a5ec3c9caef91654683fae
Golveira <gabriel.golveira@gmail.com>
2023-09-13 18:06:47 -0300

    Change classes name of badge component

--- resources/views/components/badge.blade.php
+++ resources/views/components/badge.blade.php
@@ -1,15 +1,13 @@
-@props(['color' => 'default'])
+@props(['color' => 'primary'])
 
 @php
     $classes = [
-        'default' => 'bg-blue-100 text-blue-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'primary' => 'bg-blue-100 text-blue-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'success' => 'bg-green-100 text-green-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'warning' => 'bg-yellow-100 text-yellow-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'danger' => 'bg-red-100 text-red-800 font-medium mr-2 px-2.5 py-0.5 rounded',
+        'info' => 'bg-indigo-100 text-indigo-800 font-medium mr-2 px-2.5 py-0.5 rounded',
         'dark' => 'bg-gray-100 text-gray-800 font-medium mr-2 px-2.5 py-0.5 rounded',
-        'red' => 'bg-red-100 text-red-800 font-medium mr-2 px-2.5 py-0.5 rounded',
-        'green' => 'bg-green-100 text-green-800 font-medium mr-2 px-2.5 py-0.5 rounded',
-        'yellow' => 'bg-yellow-100 text-yellow-800 font-medium mr-2 px-2.5 py-0.5 rounded',
-        'indigo' => 'bg-indigo-100 text-indigo-800 font-medium mr-2 px-2.5 py-0.5 rounded',
-        'purple' => 'bg-purple-100 text-purple-800 font-medium mr-2 px-2.5 py-0.5 rounded',
-        'pink' => 'bg-pink-100 text-pink-800 font-medium mr-2 px-2.5 py-0.5 rounded',
     ][$color];
 @endphp
 
