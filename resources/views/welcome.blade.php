508d0b5d41edab3863451bb920c10d2166a61609
Golveira <gabriel.golveira@gmail.com>
2023-09-14 11:16:24 -0300

    Update buttons in welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,28 @@
 <x-app-layout>
     <!-- Hero -->
     <section class="bg-gray-50">
-        <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 sm:grid-cols-1">
-            <div class="lg:mr-auto sm:mr-0 place-self-center lg:col-span-7 lg:text-start text-center">
-                <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
+        <div class="mx-auto grid max-w-screen-xl px-4 py-8 sm:grid-cols-1 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
+            <div class="place-self-center text-center sm:mr-0 lg:col-span-7 lg:mr-auto lg:text-start">
+                <h1 class="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl">
                     {{ __('Find your new best friend') }}
                 </h1>
 
-                <p class="font-light text-gray-500 sm:text-xl mb-5">
+                <p class="mb-5 font-light text-gray-500 sm:text-xl">
                     {{ __('We believe that every pet deserves a loving home. Adopt and make the difference.') }}
                 </p>
 
-                <div class="flex lg:justify-start justify-center items-center">
-                    <x-button href="{{ route('pets.index') }}">
+                <div class="flex items-center justify-center lg:justify-start">
+                    <x-buttons.primary-button href="{{ route('pets.index') }}">
                         {{ __('Adopt a pet') }}
-                    </x-button>
+                    </x-buttons.primary-button>
 
-                    <x-button color="alternative" href="{{ route('pets.create') }}">
+                    <x-buttons.secondary-button href="{{ route('pets.create') }}">
                         {{ __('Promote a pet') }}
-                    </x-button>
+                    </x-buttons.secondary-button>
                 </div>
             </div>
 
-            <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+            <div class="hidden lg:col-span-5 lg:mt-0 lg:flex">
                 <img src="{{ asset('assets/images/pets.png') }}" alt="mockup">
             </div>
         </div>
@@ -30,9 +30,9 @@
 
     <!-- Latest Pets -->
     <section class="bg-white">
-        <div class="py-8 px-4 mx-auto max-w-screen-xl  lg:py-16 lg:px-6">
-            <div class="mx-auto mb-8 max-w-screen-sm lg:mb-16 text-center">
-                <h2 class="mb-4 lg:text-4xl text-3xl tracking-tight font-extrabold text-gray-900">
+        <div class="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+            <div class="mx-auto mb-8 max-w-screen-sm text-center lg:mb-16">
+                <h2 class="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 lg:text-4xl">
                     {{ __('Latest pets for adoption') }}
                 </h2>
 
@@ -48,9 +48,9 @@
             </div>
 
             <div class="my-16 text-center">
-                <x-button href="{{ route('pets.index') }}">
+                <x-buttons.primary-button href="{{ route('pets.index') }}">
                     {{ __('View more') }}
-                </x-button>
+                </x-buttons.primary-button>
             </div>
         </div>
     </section>
