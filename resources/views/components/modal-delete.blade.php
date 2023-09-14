5655504e44110cbc031f2da19b9c5ceebab46ff8
Golveira <gabriel.golveira@gmail.com>
2023-09-14 12:26:57 -0300

    Add x-cloak directive to modal-delete component

--- resources/views/components/modal-delete.blade.php
+++ resources/views/components/modal-delete.blade.php
@@ -1,18 +1,27 @@
 @props(['title', 'action'])
 
-<div x-data="{ open: false }">
+@push('styles')
+    <style>
+        [x-cloak] {
+            display: none;
+        }
+    </style>
+@endpush
+
+<div x-data="{ open: false }" x-cloak>
     <div>
         {{ $slot }}
     </div>
 
-    <div x-show="open" class="z-50 fixed w-full h-full top-0 left-0 flex items-center justify-center">
-        <div x-on:click="open = false" class="absolute w-full h-full bg-gray-900 opacity-50">
+    <div class="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center" x-show="open">
+        <div class="absolute h-full w-full bg-gray-900 opacity-50" x-on:click="open = false">
         </div>
 
-        <div class="relative w-full max-w-md max-h-full">
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <button x-on:click="open = false" type="button"
-                    class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+        <div class="relative max-h-full w-full max-w-md">
+            <div class="relative rounded-lg bg-white shadow dark:bg-gray-700">
+                <button
+                    class="absolute right-2.5 top-3 ml-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+                    type="button" x-on:click="open = false">
                     <x-icons.close />
 
                     <span class="sr-only">
@@ -35,7 +44,7 @@
                             {{ __('Cancel') }}
                         </x-buttons.secondary-button>
 
-                        <x-buttons.danger-button x-on:click="open = false" type="submit">
+                        <x-buttons.danger-button type="submit" x-on:click="open = false">
                             {{ __('Delete') }}
                         </x-buttons.danger-button>
                     </form>
