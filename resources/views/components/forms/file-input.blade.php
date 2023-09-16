9fbb971fd9afcff42483cf6ad727b07f91b459ea
Golveira <gabriel.golveira@gmail.com>
2023-09-16 11:38:09 -0300

    Add validation class to file-input component

--- resources/views/components/forms/file-input.blade.php
+++ resources/views/components/forms/file-input.blade.php
@@ -1,6 +1,12 @@
-<input type="file"
-    {{ $attributes->merge([
-        'class' => 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer
-                    bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600
-                    dark:placeholder-gray-400',
-    ]) }}>
+@php
+    $classes = $errors->has($attributes->get('name'))
+        ? 'block w-full text-sm text-gray-900 border border-red-700 rounded-lg cursor-pointer
+                        bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600
+                        dark:placeholder-gray-400'
+        : 'block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer
+                        bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600
+                        dark:placeholder-gray-400';
+    
+@endphp
+
+<input type="file" {{ $attributes->merge(['class' => $classes]) }}>
