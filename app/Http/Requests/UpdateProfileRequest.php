96917ade27541dccf1b3ab13afd9bf7d08477b95
Golveira <gabriel.golveira@gmail.com>
2023-09-19 10:54:32 -0300

    Add UpdateProfileRequest

---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required',  'max:255'],
+            'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($this->user()->id)],
+            'username' => ['required', 'max:20', Rule::unique('users')->ignore($this->user()->id)],
+            'whatsapp' => ['nullable', 'max:255'],
+            'website' => ['nullable', 'max:255'],
+            'bio' => ['nullable', 'max:600'],
+            'location' => ['nullable', 'max:255'],
+            'avatar' => ['nullable', 'image', 'max:2048'],
+        ];
+    }
+}
