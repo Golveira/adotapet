743c1ec523badefaaab4f484a3f5d7fdb120b5bd
Golveira <gabriel.golveira@gmail.com>
2023-09-19 12:46:36 -0300

    Add state_id and city_id to UpdateProfileRequest

--- app/Http/Requests/UpdateProfileRequest.php
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -21,8 +21,9 @@ class UpdateProfileRequest extends FormRequest
             'username' => ['required', 'max:20', Rule::unique('users')->ignore($this->user()->id)],
             'whatsapp' => ['nullable', 'max:255'],
             'website' => ['nullable', 'max:255'],
-            'bio' => ['nullable', 'max:600'],
-            'location' => ['nullable', 'max:255'],
+            'bio' => ['nullable', 'max:500'],
+            'state_id' => ['nullable', 'exists:states,id'],
+            'city_id' => ['nullable', 'exists:cities,id'],
             'avatar' => ['nullable', 'image', 'max:2048'],
         ];
     }
