3e0211ae98b5cdaa2d6ae47da8b83f154303471a
Golveira <gabriel.golveira@gmail.com>
2023-09-19 12:38:19 -0300

    Change data type of state and city field in user table

--- database/migrations/2023_09_19_110459_add_state_field_to_users_table.php
+++ database/migrations/2023_09_19_110459_add_state_field_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('state')->nullable();
+            $table->foreignId('state_id')->nullable()->constrained();
         });
     }
 
