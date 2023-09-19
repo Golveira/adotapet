741aeeb51808dc1b8f19e112713458c74a3c5877
Golveira <gabriel.golveira@gmail.com>
2023-09-19 12:41:07 -0300

    Add state_id and city_id to UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,9 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\State;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,17 +18,20 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $state = State::inRandomOrder()->first();
+        $city = $state->cities()->inRandomOrder()->first();
+
         return [
             'name' => fake()->name(),
             'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => bcrypt('password'),
-            'website' => "https://www." . fake()->domainName(),
-            'whatsapp' => fake()->cellphoneNumber(),
+            'website' => "www." . fake()->domainName(),
+            'whatsapp' => fake()->tollFreePhoneNumber(),
             'bio' => fake()->paragraph(),
-            'state' => fake()->regionAbbr(),
-            'city' => fake()->city(),
+            'state_id' => $state->id,
+            'city_id' => $city->id,
             'remember_token' => Str::random(10),
         ];
     }
