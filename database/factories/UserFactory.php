70fd4d1d20a629dd96a51c7515d0dfaf8237905c
Golveira <gabriel.golveira@gmail.com>
2023-09-19 11:45:16 -0300

    Update UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,8 +22,12 @@ class UserFactory extends Factory
             'username' => fake()->unique()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-            // password
+            'password' => bcrypt('password'),
+            'website' => "https://www." . fake()->domainName(),
+            'whatsapp' => fake()->cellphoneNumber(),
+            'bio' => fake()->paragraph(),
+            'state' => fake()->regionAbbr(),
+            'city' => fake()->city(),
             'remember_token' => Str::random(10),
         ];
     }
