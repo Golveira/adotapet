55fab81a3058f25148959c9c8bad16163b36537b
Golveira <gabriel.golveira@gmail.com>
2023-09-21 16:21:18 -0300

    Fix SociabilityFactory and TemperamentFactory

--- database/factories/TemperamentFactory.php
+++ database/factories/TemperamentFactory.php
@@ -17,7 +17,7 @@ class TemperamentFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->word,
+            'name' => $this->faker->word(),
         ];
     }
 }
