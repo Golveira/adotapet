427a6bd3f5a69fe1649920ef4840058effee7821
Golveira <gabriel.golveira@gmail.com>
2023-09-21 10:44:15 -0300

    Add createPet and createUser to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,11 @@
 
 namespace Tests;
 
+use App\Models\Pet;
+use App\Models\User;
 use Database\Seeders\CityTableSeeder;
-use Database\Seeders\SociabilitySeeder;
 use Database\Seeders\StateTableSeeder;
+use Database\Seeders\SociabilitySeeder;
 use Database\Seeders\TemperamentSeeder;
 use Database\Seeders\VeterinaryCareSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
@@ -22,5 +24,15 @@ abstract class TestCase extends BaseTestCase
         $this->seed(SociabilitySeeder::class);
         $this->seed(TemperamentSeeder::class);
         $this->seed(VeterinaryCareSeeder::class);
+    }
+
+    protected function createPet($overrides = []): Pet
+    {
+        return Pet::factory()->create($overrides);
+    }
+
+    protected function createUser($overrides = []): User
+    {
+        return User::factory()->create($overrides);
     }
 }
