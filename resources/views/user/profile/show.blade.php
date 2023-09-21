bba74dae80eeb77532f1a3a791701aaf1273383a
Golveira <gabriel.golveira@gmail.com>
2023-09-21 10:27:59 -0300

    Refactor profile show view

--- resources/views/user/profile/show.blade.php
+++ resources/views/user/profile/show.blade.php
@@ -1,61 +1,63 @@
 <x-app-layout>
     <div class="mx-auto max-w-screen-xl px-4 py-8">
         <x-card class="mb-10 p-10">
-            <div class="grid lg:grid-cols-6 gap-4">
-                <div class="md:col-span-1 col-span-6">
-                    <x-avatar :avatar="$user->avatar" class="md:w-36 md:h-36 w-20 h-20 rounded ring-2 ring-gray-300" />
+            <div class="grid gap-4 lg:grid-cols-6">
+                <div class="col-span-6 md:col-span-1">
+                    <x-avatar class="h-20 w-20 rounded-full md:h-36 md:w-36" :avatar="$user->avatar" />
                 </div>
 
-                <div class="col-span-5 grid lg:grid-cols-2 gap-4">
-                    <div class="md:col-span-1 col-span-2">
-                        <h3 class="text-3xl font-bold text-blue-700 mb-2">
+                <div class="col-span-5 grid gap-4 lg:grid-cols-2">
+                    <div class="col-span-2 md:col-span-1">
+                        <h3 class="mb-2 text-3xl font-bold text-blue-700">
                             {{ $user->name }}
                         </h3>
 
-                        <div class="flex text-sm text-gray-600">
-                            <span class="mr-1">
-                                <x-icons.geo />
-                            </span>
+                        @if ($user->address)
+                            <div class="flex text-sm text-gray-600">
+                                <span class="mr-1">
+                                    <x-icons.geo />
+                                </span>
 
-                            {{ $user->profile?->location }}
-                        </div>
+                                {{ $user->address }}
+                            </div>
+                        @endif
                     </div>
 
-                    <div class="md:col-span-1 col-span-2">
-                        @if ($user->profile?->website)
-                            <div class="flex mb-2">
-                                <span class="text-blue-700 mr-3">
+                    <div class="col-span-2 md:col-span-1">
+                        @if ($user->website)
+                            <div class="mb-2 flex">
+                                <span class="mr-3 text-blue-700">
                                     <x-icons.globe />
                                 </span>
 
-                                <x-link href="{{ $user->profile->website }}" target="_blank"
-                                    class="text-sm text-gray-700 hover:no-underline hover:text-blue-700">
-                                    {{ $user->profile->website }}
+                                <x-link class="text-sm text-gray-700 hover:text-blue-700 hover:no-underline"
+                                    href="{{ $user->website }}" target="_blank">
+                                    {{ $user->website }}
                                 </x-link>
                             </div>
                         @endif
 
-                        @if ($user->profile?->whatsapp)
-                            <div class="flex mb-2">
-                                <span class="text-blue-700 mr-3">
+                        @if ($user->whatsapp)
+                            <div class="mb-2 flex">
+                                <span class="mr-3 text-blue-700">
                                     <x-icons.phone />
                                 </span>
 
-                                <x-link href="https://wa.me/{{ $user->profile->whatsapp }}"
-                                    class="text-sm text-gray-700 hover:no-underline hover:text-blue-700">
-                                    {{ $user->profile->whatsapp }}
+                                <x-link class="text-sm text-gray-700 hover:text-blue-700 hover:no-underline"
+                                    href="https://wa.me/{{ $user->whatsapp }}">
+                                    {{ $user->whatsapp }}
                                 </x-link>
                             </div>
                         @endif
 
                         @if ($user->email)
                             <div class="flex">
-                                <span class="text-blue-700 mr-3">
+                                <span class="mr-3 text-blue-700">
                                     <x-icons.envelope />
                                 </span>
 
-                                <x-link href="mailto:{{ $user->email }}"
-                                    class="text-sm text-gray-700 hover:no-underline hover:text-blue-700">
+                                <x-link class="text-sm text-gray-700 hover:text-blue-700 hover:no-underline"
+                                    href="mailto:{{ $user->email }}">
                                     {{ $user->email }}
                                 </x-link>
                             </div>
@@ -64,7 +66,7 @@
 
                     <div class="col-span-2">
                         <p class="text-sm text-gray-600">
-                            {{ $user->profile?->bio }}
+                            {{ $user->bio }}
                         </p>
                     </div>
                 </div>
