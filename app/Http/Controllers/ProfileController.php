fe58421dd36e88132002dffae7d6b29d0e1b1f5c
Golveira <gabriel.golveira@gmail.com>
2023-09-21 10:10:03 -0300

    Refactor ProfileController

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,65 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\View\View;
-use Illuminate\Http\Request;
-use App\Services\ProfileService;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Redirect;
-use App\Http\Requests\ProfileUpdateRequest;
 
 class ProfileController extends Controller
 {
-    public function __construct(private ProfileService $profileService)
-    {
-    }
-
     public function show(string $username): View
     {
-        $user = User::where('username', $username)
-            ->with(['profile'])
-            ->firstOrFail();
+        $user = User::where('username', $username)->firstOrFail();
 
-        return view('profile.show', compact('user'));
-    }
-
-    public function edit(Request $request): View
-    {
-        return view('profile.edit', [
-            'user' => $request->user(),
-        ]);
-    }
-
-    public function update(ProfileUpdateRequest $request): RedirectResponse
-    {
-        $this->profileService->update(
-            $request->validated(),
-            $request->file('avatar'),
-            $request->user()
-        );
-
-        toast(__('profile.updated'), 'success');
-
-        return Redirect::route('profile.show', $request->user()->username);
-    }
-
-    public function destroy(Request $request): RedirectResponse
-    {
-        $request->validateWithBag('userDeletion', [
-            'password' => ['required', 'current_password'],
-        ]);
-
-        $user = $request->user();
-
-        Auth::logout();
-
-        $user->delete();
-
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-
-        toast(__('profile.deleted'), 'success');
-
-        return Redirect::to('/');
+        return view('user.profile.show', compact('user'));
     }
 }
