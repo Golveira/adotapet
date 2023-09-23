1729d0da5c944fce0afec1cb03058b3abc1ef63b
Golveira <gabriel.golveira@gmail.com>
2023-09-23 12:04:31 -0300

    Add Favorite model

---
+++ app/Models/Favorite.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Favorite extends Model
+{
+    use HasFactory;
+
+    public function pet(): BelongsTo
+    {
+        return $this->belongsTo(Pet::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
