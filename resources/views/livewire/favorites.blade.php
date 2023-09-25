c59ca4a95d3f82afaf6119b3c3da98cb02acc9d7
Golveira <gabriel.golveira@gmail.com>
2023-09-25 11:56:48 -0300

    Format css classes of favorites view

--- resources/views/livewire/favorites.blade.php
+++ resources/views/livewire/favorites.blade.php
@@ -1,15 +1,15 @@
 <section>
-    <div class="py-8 px-4 mx-auto max-w-screen-xl">
-        <h2 class="mb-8 lg:text-2xl text-xl text-center tracking-tight font-extrabold text-gray-900">
+    <div class="mx-auto max-w-screen-xl px-4 py-8">
+        <h2 class="mb-8 text-center text-xl font-extrabold tracking-tight text-gray-900 lg:text-2xl">
             {{ __('Favorites pets') }}
         </h2>
 
         <div class="grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             @forelse ($pets as $pet)
-                <livewire:pet-card wire:key="item-{{ $pet->id }}" :pet="$pet" />
+                <livewire:pet-card :pet="$pet" :wire:key="$pet->id" />
             @empty
-                <div class="mt-10 col-span-4 text-center">
-                    <h3 class="text-xl mb-3"> {{ __("You don't have favorite pets yet") }}</h3>
+                <div class="col-span-4 mt-10 text-center">
+                    <h3 class="mb-3 text-xl"> {{ __("You don't have favorite pets yet") }}</h3>
 
                     <p class="mb-6 text-gray-600">
                         {{ __('You can search for pets and add them to your favorites') }}
