3bbc7d179c18e5c8036551d5861f7d1529071e03
Golveira <gabriel.golveira@gmail.com>
2023-09-26 11:37:54 -0300

    Rename primary-link to dark-link

---
+++ resources/views/components/links/dark-link.blade.php
@@ -0,0 +1,9 @@
+@props(['active'])
+
+@php
+    $classes = $active ?? false ? 'block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0' : 'block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0';
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes]) }}>
+    {{ $slot }}
+</a>
