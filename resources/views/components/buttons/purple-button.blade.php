95b9ff948c55669b938f8626777fd39c2e1e55c0
Golveira <gabriel.golveira@gmail.com>
2023-09-26 12:16:06 -0300

    Add purple button

---
+++ resources/views/components/buttons/purple-button.blade.php
@@ -0,0 +1,18 @@
+@props(['outline' => false])
+
+@php
+    $classes = [
+        'solid' => 'mr-2 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900',
+        'outline' => 'text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-purple-400 dark:text-purple-400 dark:hover:text-white dark:hover:bg-purple-500 dark:focus:ring-purple-900',
+    ][$outline ? 'outline' : 'solid'];
+@endphp
+
+@if ($attributes->has('href'))
+    <a {{ $attributes->merge(['class' => $classes]) }}>
+        {{ $slot }}
+    </a>
+@else
+    <button {{ $attributes->merge(['class' => $classes]) }}>
+        {{ $slot }}
+    </button>
+@endif
