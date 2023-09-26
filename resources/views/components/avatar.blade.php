4a41f4b8756fb6daee266424708db89009dc6c27
Golveira <gabriel.golveira@gmail.com>
2023-09-26 10:27:31 -0300

    Update avatar component

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,4 +1,4 @@
 @props(['avatar' => null])
 
-<img {{ $attributes->merge(['class' => 'object-cover w-10 h-10 ring-2 ring-gray ring-2 ring-gray']) }}
-    src="{{ $avatar }}" alt="User avatar">
+<img src="{{ $avatar }}" alt="User avatar"
+    {{ $attributes->merge(['class' => 'object-cover p-1 w-10 h-10 ring-2 ring-gray-300']) }}>
