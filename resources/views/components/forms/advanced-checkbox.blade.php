94f898b13ba0e9734e48b85b8e5a83582dcc558f
Golveira <gabriel.golveira@gmail.com>
2023-09-26 12:05:15 -0300

    Format advanced-checkbox

--- resources/views/components/forms/advanced-checkbox.blade.php
+++ resources/views/components/forms/advanced-checkbox.blade.php
@@ -1,10 +1,11 @@
 @props(['id', 'name', 'value', 'label', 'checked' => false])
 
-<input class="hidden peer" type="checkbox" id="{{ $id }}" name="{{ $name }}" value="{{ $value }}"
+<input class="peer hidden" id="{{ $id }}" name="{{ $name }}" type="checkbox" value="{{ $value }}"
     {{ $checked ? 'checked' : '' }}>
 
-<label for="{{ $id }}"
-    class="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border-2 border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-600 hover:text-gray-600  peer-checked:text-gray-600 hover:bg-gray-50">
+<label
+    class="inline-flex w-full cursor-pointer items-center justify-between rounded-lg border-2 border-gray-200 bg-white p-2 text-gray-500 hover:bg-gray-50 hover:text-gray-600 peer-checked:border-blue-600 peer-checked:text-gray-600"
+    for="{{ $id }}">
 
     <span class="flex items-center">
         <span class="text-sm font-medium">{{ __($label) }}</span>
