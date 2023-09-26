9648074f8051daaaff040329201396c91c1a691b
Golveira <gabriel.golveira@gmail.com>
2023-09-26 09:54:20 -0300

    Add application-logo to footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,33 +1,33 @@
-<footer class="p-4 bg-gray-100 sm:p-6 dark:bg-gray-800 sticky top-[100vh]">
+<footer class="sticky top-[100vh] bg-gray-100 p-4 dark:bg-gray-800 sm:p-6">
     <div class="mx-auto max-w-screen-xl">
         <div class="md:flex md:justify-between">
             <!-- Logo -->
-            <a href="/">
-                <img src="{{ asset('assets/images/logo.png') }}" class="mr-3" alt="Adotapet Logo" />
+            <a class="mb-10 block" href="{{ route('welcome') }}">
+                <x-application-logo />
             </a>
 
             <!-- Links -->
-            <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <div class="grid grid-cols-2 gap-8 sm:grid-cols-3 sm:gap-6">
                 <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                    <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
                         Pets
                     </h2>
 
                     <ul class="text-gray-600 dark:text-gray-400">
                         <li class="mb-4">
-                            <a href="{{ route('pets.index') }}" class="hover:underline">
+                            <a class="hover:underline" href="{{ route('pets.index') }}">
                                 {{ __('Adopt a pet') }}
                             </a>
                         </li>
 
                         <li class="mb-4">
-                            <a href="{{ route('pets.create') }}" class="hover:underline">
+                            <a class="hover:underline" href="{{ route('pets.create') }}">
                                 {{ __('Promote a pet') }}
                             </a>
                         </li>
 
                         <li>
-                            <a href="#" class="hover:underline">
+                            <a class="hover:underline" href="#">
                                 {{ __('Make a donation') }}
                             </a>
                         </li>
@@ -35,25 +35,25 @@
                 </div>
 
                 <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                    <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
                         {{ __('Profile') }}
                     </h2>
 
                     <ul class="text-gray-600 dark:text-gray-400">
                         <li class="mb-4">
                             @auth
-                                <a href="{{ route('profile.show', Auth::user()->username) }}" class="hover:underline ">
+                                <a class="hover:underline" href="{{ route('profile.show', Auth::user()->username) }}">
                                     {{ __('My Profile') }}
                                 </a>
                             @endauth
                         </li>
                         <li class="mb-4">
-                            <a href="{{ route('login') }}" class="hover:underline">
+                            <a class="hover:underline" href="{{ route('login') }}">
                                 {{ __('Log in') }}
                             </a>
                         </li>
                         <li>
-                            <a href="{{ route('register') }}" class="hover:underline">
+                            <a class="hover:underline" href="{{ route('register') }}">
                                 {{ __('Register') }}
                             </a>
                         </li>
@@ -61,23 +61,23 @@
                 </div>
 
                 <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                    <h2 class="mb-6 text-sm font-semibold uppercase text-gray-900 dark:text-white">
                         {{ __('Adotapet') }}
                     </h2>
 
                     <ul class="text-gray-600 dark:text-gray-400">
                         <li class="mb-4">
-                            <a href="#" class="hover:underline">
+                            <a class="hover:underline" href="#">
                                 {{ __('About us') }}
                             </a>
                         </li>
                         <li class="mb-4">
-                            <a href="#" class="hover:underline">
+                            <a class="hover:underline" href="#">
                                 {{ __('Privacy Policy') }}
                             </a>
                         </li>
                         <li>
-                            <a href="#" class="hover:underline">
+                            <a class="hover:underline" href="#">
                                 {{ __('Terms and Conditions') }}
                             </a>
                         </li>
@@ -86,11 +86,11 @@
             </div>
         </div>
 
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr class="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
 
         <div class="sm:flex sm:items-center sm:justify-between">
             <!-- Copyright -->
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            <span class="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
                 © 2023 <a href="#"class="hover:underline">
                     {{ __('Adotapet') }}
                 </a>.
@@ -98,16 +98,16 @@
             </span>
 
             <!-- Social media -->
-            <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-                <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+            <div class="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+                <a class="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="#">
                     <x-icons.facebook />
                 </a>
 
-                <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <a class="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="#">
                     <x-icons.instagram />
                 </a>
 
-                <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
+                <a class="text-gray-500 hover:text-gray-900 dark:hover:text-white" href="#">
                     <x-icons.twitter />
                 </a>
             </div>
