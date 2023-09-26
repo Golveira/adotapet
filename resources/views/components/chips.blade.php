d1f28e78d228fcedb7e09bec42d2a38c95c5b50c
Golveira <gabriel.golveira@gmail.com>
2023-09-26 10:28:41 -0300

    Format css classes of blade components

--- resources/views/components/chips.blade.php
+++ resources/views/components/chips.blade.php
@@ -13,12 +13,12 @@
     ],
 ])
 
-<span class="inline-flex items-center px-2 py-1 mr-2 text-sm font-medium rounded {{ $colors[$color] }}"
+<span class="{{ $colors[$color] }} mr-2 inline-flex items-center rounded px-2 py-1 text-sm font-medium"
     {{ $attributes }}>
     {{ $slot }}
-    <button type="button" class="inline-flex items-center p-1 ml-2 text-sm bg-transparent rounded-sm"
-        data-dismiss-target="#badge-dismiss-default" aria-label="Remove">
-        <svg class="w-2 h-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+    <button class="ml-2 inline-flex items-center rounded-sm bg-transparent p-1 text-sm"
+        data-dismiss-target="#badge-dismiss-default" type="button" aria-label="Remove">
+        <svg class="h-2 w-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
         </svg>
--- resources/views/components/dropzone.blade.php
+++ resources/views/components/dropzone.blade.php
@@ -1,8 +1,9 @@
-<div class="relative flex items-center justify-center w-full mb-10">
-    <label for="dropzone-file"
-        class="relative flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-        <div class="flex flex-col items-center justify-center pt-5 pb-6">
-            <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+<div class="relative mb-10 flex w-full items-center justify-center">
+    <label
+        class="dark:hover:bg-bray-800 relative flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        for="dropzone-file">
+        <div class="flex flex-col items-center justify-center pb-6 pt-5">
+            <svg class="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400" aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
@@ -19,7 +20,7 @@
             </p>
         </div>
 
-        <input type="file" id="dropzone-file" multiple
-            class="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer" {{ $attributes }} />
+        <input class="absolute left-0 top-0 h-full w-full cursor-pointer opacity-0" id="dropzone-file" type="file"
+            multiple {{ $attributes }} />
     </label>
 </div>
