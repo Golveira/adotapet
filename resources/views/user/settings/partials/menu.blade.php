37286beb946b38c6e86bf9be19b8d1b5dea571aa
Golveira <gabriel.golveira@gmail.com>
2023-09-26 11:22:29 -0300

    Rename links of  settings menu

--- resources/views/user/settings/partials/menu.blade.php
+++ resources/views/user/settings/partials/menu.blade.php
@@ -1,13 +1,13 @@
 <div class="rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900">
-    <x-list-group-link href="#update-profile">
+    <x-links.list-group-link href="#update-profile">
         {{ __('Profile') }}
-    </x-list-group-link>
+    </x-links.list-group-link>
 
-    <x-list-group-link href="#update-password">
+    <x-links.list-group-link href="#update-password">
         {{ __('Password') }}
-    </x-list-group-link>
+    </x-links.list-group-link>
 
-    <x-list-group-link href="#remove-account">
+    <x-links.list-group-link href="#remove-account">
         {{ __('Remove Account') }}
-    </x-list-group-link>
+    </x-links.list-group-link>
 </div>
