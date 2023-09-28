154dd27ced7d54a08c3a08ec5cf6467a83a1a27c
Golveira <gabriel.golveira@gmail.com>
2023-09-28 12:55:02 -0300

    Refactor verify-email page

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,10 +1,10 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        {{ __("Before getting started, you must verify your email address by clicking on the verification link sent tou your email. If you didn't receive verification link, check your spam folder or request a new link.") }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
+        <div class="mb-4 text-sm font-medium text-green-600">
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}
         </div>
     @endif
@@ -14,18 +14,18 @@
             @csrf
 
             <div>
-                <x-primary-button>
+                <x-buttons.primary-button>
                     {{ __('Resend Verification Email') }}
-                </x-primary-button>
+                </x-buttons.primary-button>
             </div>
         </form>
 
         <form method="POST" action="{{ route('logout') }}">
             @csrf
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <x-buttons.dark-button type="submit">
                 {{ __('Log Out') }}
-            </button>
+            </x-buttons.dark-button>
         </form>
     </div>
 </x-guest-layout>
