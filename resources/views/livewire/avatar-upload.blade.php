7ff1a17efde679555a247da826cc6104de7e287a
Golveira <gabriel.golveira@gmail.com>
2023-09-28 11:33:39 -0300

    Fix button icon of avatar-upload

--- resources/views/livewire/avatar-upload.blade.php
+++ resources/views/livewire/avatar-upload.blade.php
@@ -1,6 +1,6 @@
-<div class="p-4 mb-6 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 ">
+<div class="mb-6 rounded-lg border border-gray-200 bg-white p-4 shadow-sm 2xl:col-span-2">
     <div class="flex gap-8">
-        <img class="mb-4 rounded-lg object-cover w-28 h-28 sm:mb-0 xl:mb-4 2xl:mb-0" src="{{ $user->avatar }}"
+        <img class="mb-4 h-28 w-28 rounded-lg object-cover sm:mb-0 xl:mb-4 2xl:mb-0" src="{{ $user->avatar }}"
             alt="Avatar">
 
         <div>
@@ -16,17 +16,19 @@
                 <span class="text-sm text-red-500">{{ $message }}</span>
             @enderror
 
-            <div class="mt-4 flex flex-col md:flex-row gap-2">
-                <label for="photo"
-                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 cursor-pointer">
-                    <x-icons.upload />
+            <div class="mt-4 flex flex-col gap-2 md:flex-row">
+                <label
+                    class="inline-flex cursor-pointer items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white"
+                    for="photo">
+                    <x-icons.upload class="mr-3 h-4 w-4" />
                     {{ __('Upload Picture') }}
                 </label>
 
-                <input wire:model="photo" type="file" class="hidden" name="photo" id="photo">
+                <input class="hidden" id="photo" name="photo" type="file" wire:model="photo">
 
-                <button wire:click="removeAvatar" type="button"
-                    class="py-2 px-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200">
+                <button
+                    class="rounded-lg border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200"
+                    type="button" wire:click="removeAvatar">
                     {{ __('Remove') }}
                 </button>
             </div>
