99d28e1d48668d731265967eb66d827e7d42af39
Golveira <gabriel.golveira@gmail.com>
2023-09-29 11:02:07 -0300

    Add email verification button to settings page

--- resources/views/user/settings/partials/email-verification-message.blade.php
+++ resources/views/user/settings/partials/email-verification-message.blade.php
@@ -1,15 +1,19 @@
 @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
     <div class="mb-6">
         <x-alert type="warning">
-            <p class="mt-2 text-sm text-gray-800">
-                {{ __('Your email address is unverified.') }}
+            <div class="flex flex-col items-center text-sm text-gray-800 md:flex-row">
+                <p class="mr-1">
+                    {{ __('Your email address is unverified.') }}
+                </p>
 
-                <button
-                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                    form="send-verification">
-                    {{ __('Click here to re-send the verification email.') }}
-                </button>
-            </p>
+                <form method="POST" action="{{ route('verification.send') }}">
+                    @csrf
+
+                    <button class="rounded-md text-sm text-gray-600 underline hover:text-gray-900" type="submit">
+                        {{ __('Click here to re-send the verification email.') }}
+                    </button>
+                </form>
+            </div>
 
             @if (session('status') === 'verification-link-sent')
                 <p class="mt-2 text-sm font-medium text-green-600">
