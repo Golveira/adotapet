da47ef424cf189a5c5ff8194381a19848e5d9fab
Golveira <gabriel.golveira@gmail.com>
2023-10-03 10:46:19 -0300

    Refactor sidebar of admin area

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,27 +1,27 @@
-<aside id="sidebar"
-    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
-    aria-label="Sidebar">
-    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
-        <ul class="space-y-2 font-medium">
+<aside
+    class="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full border-r border-gray-200 bg-white pt-14 transition-transform dark:border-gray-700 dark:bg-gray-800 md:translate-x-0"
+    id="drawer-navigation" aria-label="Sidenav">
+    <div class="h-full overflow-y-auto bg-white px-3 py-5 dark:bg-gray-800">
+        <ul class="space-y-2">
             <li>
-                <x-sidebar-link href="{{ route('admin.home') }}" :active="request()->routeIs('admin.home')">
+                <x-links.sidebar-link :href="route('admin.home')" :active="request()->routeIs('admin.home')">
                     <x-icons.chart-pie />
                     <span class="ml-3">Dashboard</span>
-                </x-sidebar-link>
+                </x-links.sidebar-link>
             </li>
 
             <li>
-                <x-sidebar-link href="{{ route('admin.users.index') }}" :active="request()->routeIs('admin.users.index')">
+                <x-links.sidebar-link :href="route('admin.users.index')" :active="request()->routeIs('admin.users.index')">
                     <x-icons.users />
                     <span class="ml-3">{{ __('Users') }}</span>
-                </x-sidebar-link>
+                </x-links.sidebar-link>
             </li>
 
             <li>
-                <x-sidebar-link href="{{ route('admin.pets.index') }}" :active="request()->routeIs('admin.pets.index')">
+                <x-links.sidebar-link :href="route('admin.pets.index')" :active="request()->routeIs('admin.pets.index')">
                     <x-icons.paw />
-                    <span class="ml-3">Pets</span>
-                </x-sidebar-link>
+                    <span class="ml-3">{{ __('Pets') }}</span>
+                </x-links.sidebar-link>
             </li>
         </ul>
     </div>
