5ac5cf6ba249927314dbc89dad6f050d2a91cd79
Golveira <gabriel.golveira@gmail.com>
2023-10-03 10:50:33 -0300

    Remove notification from navbar of admin area

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -29,9 +29,6 @@
         </div>
 
         <div class="flex items-center lg:order-2">
-            <!-- Notifications -->
-            <x-notifications />
-
             <!-- User Dropdown button -->
             <button
                 class="mx-3 flex rounded-full text-sm focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600 md:mr-0"
@@ -52,15 +49,6 @@
                         {{ auth()->user()->email }}
                     </span>
                 </div>
-
-                <ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
-                    <li>
-                        <a class="block px-4 py-2 text-sm hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
-                            href="#">
-                            {{ __('Settings') }}
-                        </a>
-                    </li>
-                </ul>
 
                 <ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
                     <li>
